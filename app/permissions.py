3ab57bbb0180e7f0c544dcda9720795a28a23da0
Coco <35708250+Cocorico84@users.noreply.github.com>
2021-10-26 11:48:31 +0200

    Update permissions.py

--- app/permissions.py
+++ app/permissions.py
@@ -10,4 +10,4 @@ class IsContributor(BasePermission):
 class IsAuthorOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (obj.author_user == request.user and request.user  == getattr(obj, "assignee_user", None) or request.method in SAFE_METHODS)
+        return (obj.author_user == request.user and request.user == getattr(obj, "assignee_user", request.user) or request.method in SAFE_METHODS)
