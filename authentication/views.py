262dd2612e9c0acc9266b839f5e6447007a4cebc
Corentin <corentin@fanprime.io>
2021-11-17 11:08:14 +0100

    add delete user feature

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,8 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, get_object_or_404
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.views import APIView
 
 from .models import User
 from .serializers import RegisterSerializer
@@ -7,3 +11,15 @@ from .serializers import RegisterSerializer
 class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class DeleteUserView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def delete(self, request, user_pk: int, *args, **kwargs) -> Response:
+        '''
+        This method DELETE removes a user from the database. But you can only use this method if you are admin or staff.
+        '''
+        user = get_object_or_404(User, pk=user_pk)
+        user.delete()
+        return Response('The user is deleted.', status=HTTP_200_OK)
--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from app.views import CommentViewSet, IssueViewSet, ProjectViewSet, UserViewSet
-from authentication.views import RegisterView
+from authentication.views import RegisterView, DeleteUserView
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_nested import routers
@@ -34,6 +34,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', TokenObtainPairView.as_view(), name='obtain_tokens'),
     path('signup/', RegisterView.as_view(), name='signup'),
+    path('user/delete/<int:user_pk>', DeleteUserView.as_view(), name='delete_user'),
     path('api/', include(router.urls)),
     path('api/', include(projects_router.urls)),
     path('api/', include(issues_router.urls)),
